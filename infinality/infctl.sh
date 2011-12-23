aee35f38d507bc361c04ad439a56e210ab756f52
yegorius <yegoriys@gmail.com>
2011-12-24 01:52:59 +0200

    allow style to be set in one command

--- infinality/infctl.sh
+++ infinality/infctl.sh
@@ -27,14 +27,42 @@ function populatedirs {
 	return 0
 }
 
+function showstyles {
+	printf "\tavailable styles are: "; echo $styles
+}
+
 function selectconfd {
 	printf "Select a style:\n"
 	select style in $styles; do
+		if [[ -z "$style" ]]; then
+			printf "\tPlease select available style\n"
+			return 1
+		fi
 		ln -sfn $stylesdir/$style conf.d
 		printf "conf.d -> %s/%s\n" $stylesdir $style
 		break
 	done
 }
+
+function setconfd {
+	if [[ -z "$1" ]]; then
+		selectconfd
+		return $?
+	fi
+	
+	for style in $styles; do
+		if [[ "$1" = "$style" ]]; then
+			ln -sfn $stylesdir/$style conf.d
+			printf "conf.d -> %s/%s\n" $stylesdir $style
+			return 0
+		fi
+	done
+	
+	printf "\tNo such style,\n"
+	showstyles
+	return 1
+}
+
 
 function usage {
 	printf "\tUsage:\n"
@@ -47,7 +75,7 @@ case $1 in
 	makestyles)
 		createdirs && populatedirs || exit 1 ;;
 	setstyle)
-		selectconfd ;;
+		setconfd $2 ;;
 	*)
 		usage ;;
 esac
