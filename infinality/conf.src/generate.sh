9eebce532ebc2d6a5542d1808ba23e5c81b52122
yegorius <yegorius@domic.us>
2012-09-29 14:17:59 +0300

    New method to generate 60-group-*-fonts files.

--- infinality/conf.src/generate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /etc/fonts/infinality/conf.src
-
-./generate-config-from-list.sh tt-fonts.list tt-fonts.template > 60-group-tt-fonts.conf
-./generate-config-from-list.sh non-tt-fonts.list non-tt-fonts.template > 60-group-non-tt-fonts.conf
